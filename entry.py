9cb282eec596e98599a7a0ac99540414c7593d53
VivekBits2210 <viveknayak2210@gmail.com>
2023-10-08 19:20:16 -0400

    Temporarily commented out download button

--- entry.py
+++ entry.py
@@ -130,7 +130,7 @@ def main():
 
         st.success('Processing Complete!')
         st.balloons()
-        st.download_button(label="Download Result", file_name="results.zip", file="./outputs/results.zip")
+        # st.download_button(label="Download Result")
 
 
 if __name__ == "__main__":
